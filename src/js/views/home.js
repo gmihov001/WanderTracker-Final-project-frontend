cf8c47cf809fc5873d472bb4d12b09c293d4be87
Fernando Funez <ferfunes@gmail.com>
2019-10-28 23:55:16 +0000

    rigo is out

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,9 +4,7 @@ import "../../styles/home.scss";
 export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
+		<p />
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
