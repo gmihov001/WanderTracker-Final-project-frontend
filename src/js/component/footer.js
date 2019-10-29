a1f61c2d5f5488da7be34d8e20e5f705b54ad78a
Fernando Funez <ferfunes@gmail.com>
2019-10-29 14:41:08 +0000

    navbar done

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import wtLogo from "../../img/wanderTrackerLogo.png";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center footer-light bg-light ">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">Georgi and Fernando</a>
-		</p>
+		<div className="row justify-content-center">
+			<div className="col-md-5 text-center text-secondary">
+				<img src={wtLogo} />
+				<p>
+					Made with <i className="fa fa-heart text-danger" /> by Georgi and Fernando
+				</p>
+
+				<strong>Contact Info</strong>
+				<p>
+					(888) 888-8888
+					<br />
+					email@wandertracker.com
+				</p>
+				<a href="#" target="_blank">
+					<i className="fab fa-facebook-square" />
+				</a>
+				<a href="#" target="_blank">
+					<i className="fab fa-twitter-square" />
+				</a>
+				<a href="#" target="_blank">
+					<i className="fab fa-instagram" />
+				</a>
+			</div>
+		</div>
 	</footer>
 );
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import wtLogo from "../../img/wanderTrackerLogo.png";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">WanderTracker</span>
+				<img className="logo navbar-brand mb-0 h1" src={wtLogo} />
 			</Link>
 			<div className="ml-auto">
-				<button className="btn text-light">Log In</button>
-				<button className="btn text-light ml-1">Sign Up</button>
+				<button className="btn text-secondary">Log In</button>
+				<button className="btn text-secondary ml-1">Sign Up</button>
 			</div>
 		</nav>
 	);
