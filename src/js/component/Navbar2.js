70a588818605dd1a61d55541cf67dc60b13775f7
Fernando Funez <ferfunes@gmail.com>
2019-11-05 21:21:26 +0000

    fixed  navbar2 made it  resposive and collapsible

--- src/js/component/Navbar2.js
+++ src/js/component/Navbar2.js
@@ -9,32 +9,54 @@ import MapIcon from "../../img/MapLocation.png";
 export const Navbar2 = () => {
 	return (
 		<div className="wrapper">
-			<nav className="navbar  navbar-light d-flex justify-content-center">
-				<div className="container text-center">
-					<Link to="/Stamps">
-						<img className="icons-navbar navbar-brand mb-0 h1" title="Go Stamps" src={StampsIcon} />
-						<p>Stamps</p>
-					</Link>
-					<Link to="/TravelDoc">
-						<img
-							className="icons-navbar navbar-brand mb-0 h1"
-							title="Go To Travel Documets"
-							src={DocIcon}
-						/>
-						<p>Travel Documents</p>
-					</Link>
-					<Link to="/Map">
-						<img className="icons-navbar navbar-brand mb-0 h1" title="Go To Maps" src={MapIcon} />
-						<p>Map</p>
-					</Link>
-					<Link to="/TripPlanner">
-						<img
-							className="icons-navbar navbar-brand mb-0 h1 "
-							title="Go To Travel Planner"
-							src={CalIcon}
-						/>
-						<p>Trip Planner</p>
-					</Link>
+			<nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center ">
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div className="collapse navbar-collapse  " id="navbarSupportedContent">
+					<div className="container navbar-nav mr-auto d-flex justify-content-between text-center">
+						<Link to="/Stamps">
+							<div>
+								<img className="icons-navbar navbar-brand mb-0 h1" title="Go Stamps" src={StampsIcon} />
+
+								<p>Stamps</p>
+							</div>
+						</Link>
+						<Link to="/TravelDoc">
+							<div>
+								<img
+									className="icons-navbar navbar-brand mb-0 h1"
+									title="Go To Travel Documets"
+									src={DocIcon}
+								/>
+								<p>Travel Documents</p>
+							</div>
+						</Link>
+						<Link to="/Map">
+							<div>
+								<img className="icons-navbar navbar-brand mb-0 h1" title="Go To Maps" src={MapIcon} />
+								<p>Map</p>
+							</div>
+						</Link>
+						<Link to="/TripPlanner">
+							<div>
+								<img
+									className="icons-navbar navbar-brand mb-0 h1 "
+									title="Go To Travel Planner"
+									src={CalIcon}
+								/>
+								<p>Trip Planner</p>
+							</div>
+						</Link>
+					</div>
 				</div>
 			</nav>
 		</div>
