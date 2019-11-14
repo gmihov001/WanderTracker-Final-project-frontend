c9e9530f31fb2c45a7a7df7220d5e90beacb201f
Fernando Funez <ferfunes@gmail.com>
2019-11-14 15:47:54 +0000

    change map marker from circle to pin

--- src/js/component/SimpleMap.js
+++ src/js/component/SimpleMap.js
@@ -14,10 +14,16 @@ export const SimpleMap = ({ markers }) => {
 				</Geographies>
 				{markers.map((marker, index) => (
 					<Marker key={index} coordinates={[marker[1], marker[0]]}>
-						<circle r={10} fill="#F00" stroke="#fff" strokeWidth={2} />
-						<text textAnchor="middle" style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}>
-							{index}
-						</text>
+						<g
+							fill="none"
+							stroke="#FF5533"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							transform="translate(-12, -24)">
+							<circle cx="12" cy="10" r="3" />
+							<path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+						</g>
 					</Marker>
 				))}
 			</ComposableMap>
